7737bb3b691d22bdb3d5adc467ae6f7459eec98c
LeeJSYS <dauzzang@gmail.com>
2023-06-12 16:54:06 +0900

    lala

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi, my name is JSL
+          Learn React
         </a>
       </header>
     </div>
